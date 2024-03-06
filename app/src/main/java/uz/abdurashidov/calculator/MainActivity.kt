f9dffa0e2ed4aba0afe5508e3bd109a4266c191e
abdurashidovvv <abdurashidovsaydullo35@gmail.com>
2024-03-06 11:15:58 +0000

    ScreenCalc

--- app/src/main/java/uz/abdurashidov/calculator/MainActivity.kt
+++ app/src/main/java/uz/abdurashidov/calculator/MainActivity.kt
@@ -3,15 +3,25 @@ package uz.abdurashidov.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import uz.abdurashidov.calculator.presentation.component.CaclButtonComponent
+import uz.abdurashidov.calculator.presentation.component.InputDisplayComponent
 import uz.abdurashidov.calculator.presentation.theme.ButtonPink
 import uz.abdurashidov.calculator.presentation.theme.CalculatorTheme
+import uz.abdurashidov.calculator.presentation.theme.spacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +33,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
                 }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ScreenCalc() {
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier.padding(
+                horizontal = MaterialTheme.spacing.lg,
+                vertical = MaterialTheme.spacing.sm
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                InputDisplayComponent(result = "0")
+                Spacer(modifier = Modifier.height(MaterialTheme.spacing.lg))
             }
         }
     }
